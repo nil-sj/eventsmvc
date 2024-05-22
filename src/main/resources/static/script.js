5b5f4c0a90959a11c2379f17ea1d01703b227738
Nil SJ <niladri123@gmail.com>
2024-05-22 14:55:49 -0400

    Added static resources

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page Loaded");
+    document.querySelector('body').style.backgroundColor = "#f3f3f3";
+});
+
+// may not work because I see the following notification at the top of my IntelliJ pane
+// *.js files are supported by IntelliJ Idea Ultimate | Try InetlliJ Idea Ultimate
