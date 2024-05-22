64b48a9431dd36c549a5d6cdba41481af8543a12
Nil SJ <niladri123@gmail.com>
2024-05-22 12:55:55 -0400

    Added form and form handler

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,21 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
     @GetMapping
     public String events(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Devs Meetup");
-        events.add("Code 2gether");
-        events.add("WebDev Workshop");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    @GetMapping("create")
+    public String eventCreate() {
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String addEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 }
