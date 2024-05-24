fdd90fbd8d1eaa3d8fc0490a9cb6bb82d5297c55
Nil SJ <niladri123@gmail.com>
2024-05-23 22:42:36 -0400

    Added validation to model

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,22 +1,29 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
+    @NotBlank(message="Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
-    public Event() {
-        id = nextId;
-        nextId++;
-    }
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
-        this();
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.id = nextId;
+        nextId++;
     }
 
     public int getId() {
@@ -38,6 +45,10 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getContactEmail() { return contactEmail; }
+
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
 
     @Override
     public String toString() {
