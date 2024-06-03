469eb3ef364d64301916456ea237c677bd2989f0
Niladri Sekhar Jyoti <nil@Niladris-MacBook-Pro.local>
2024-06-03 13:29:47 -0400

    Created One to One Relationship

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,10 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 
 @Entity
@@ -13,27 +16,16 @@ public class Event extends AbstractEntity{
     @ManyToOne
     @NotNull(message = "Event category is required.")
     private EventCategory eventCategory;
-    @Size(max = 500, message = "Description too long!")
-    private String description;
-    @NotBlank(message="Location cannot be left blank.")
-    private String location;
-    @AssertTrue(message="Attendees must register for the event.")
-    private boolean shouldRegister;
-    @Positive(message="Number of attendees must be one or more.")
-    private int numberOfAttendees;
-    @Email(message = "Invalid email. Try again.")
-    private String contactEmail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
 
     public Event() {}
 
-    public Event(String name, String description, String location, boolean shouldRegister, int numberOfAttendees, String contactEmail, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
         this.eventCategory = eventCategory;
-        this.description = description;
-        this.location = location;
-        this.shouldRegister = shouldRegister;
-        this.numberOfAttendees = numberOfAttendees;
-        this.contactEmail = contactEmail;
     }
 
     public String getName() {
@@ -48,29 +40,9 @@ public class Event extends AbstractEntity{
 
     public void setEventCategory(EventCategory eventCategory) { this.eventCategory = eventCategory; }
 
-    public String getDescription() {
-        return description;
-    }
+    public EventDetails getEventDetails() { return eventDetails; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getLocation() { return location; }
-
-    public void setLocation(String location) { this.location = location; }
-
-    public boolean isShouldRegister() { return shouldRegister; }
-
-    public void setShouldRegister(boolean shouldRegister) { this.shouldRegister = shouldRegister; }
-
-    public int getNumberOfAttendees() { return numberOfAttendees; }
-
-    public void setNumberOfAttendees(int numberOfAttendees) { this.numberOfAttendees = numberOfAttendees; }
-
-    public String getContactEmail() { return contactEmail; }
-
-    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
+    public void setEventDetails(EventDetails eventDetails) { this.eventDetails = eventDetails; }
 
     @Override
     public String toString() {
